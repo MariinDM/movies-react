a6f0ea75f0951e737d9ae651822bb28f4673f785
MariinDM <diegomaarinmontalvo@gmail.com>
2025-10-08 16:15:44 -0600

    [UPD] Update button labels in Carousel component to Spanish

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -205,10 +205,10 @@ const CarouselComponent = () => {
                         >
                           <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
                         </svg>
-                        Watch Now
+                        Ver Trailer
                       </button>
                       <button className="bg-gray-600/80 text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-700/80 transition-colors">
-                        More Info
+                        Más Información
                       </button>
                     </div>
                   </div>
