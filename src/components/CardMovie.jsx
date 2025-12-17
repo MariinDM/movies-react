41af803f347df294e2b7520627eb85a920443ce1
MariinDM <diegomaarinmontalvo@gmail.com>
2025-12-17 10:29:32 -0600

    [ADD] Implement favorite functionality in CardMovie component; add HeartIcon for visual feedback; refactor TrendingMovies to use CardMovie

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,7 +1,22 @@
+import { HeartIcon } from "@components/Icons";
+import { useState } from "react";
+
 const CardMovie = ({ movie }) => {
   const PATH_IMAGE = "https://image.tmdb.org/t/p/w500";
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const addFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
-    <div key={movie.id} className="bg-gray-800 rounded-lg p-4">
+    <div key={movie.id} className="relative bg-gray-800 rounded-lg p-4">
+      <button
+        onClick={addFavorite}
+        className="absolute top-5 right-5 z-10 w-6 h-6"
+      >
+        <HeartIcon filled={isFavorite} className="w-6 h-6" />
+      </button>
       <img
         src={PATH_IMAGE + movie.poster_path}
         alt={movie.title}
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,5 +1,6 @@
 import movieService from "@/services/movie-service";
 import { useEffect, useState } from "react";
+import CardMovie from "./CardMovie";
 
 const TrendingMovies = () => {
   const [movies, setMovies] = useState([]);
@@ -25,16 +26,8 @@ const TrendingMovies = () => {
       </h2>
 
       <section className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        {/* Aquí se mapearían las películas obtenidas */}
         {movies.map((movie) => (
-          <div key={movie.id} className="bg-gray-800 rounded-lg p-4">
-            <img
-              src={movie.image}
-              alt={movie.title}
-              className="w-full h-auto rounded-md mb-2"
-            />
-            <h3 className="text-white text-center">{movie.title}</h3>
-          </div>
+          <CardMovie key={movie.id} movie={movie} />
         ))}
       </section>
     </div>
